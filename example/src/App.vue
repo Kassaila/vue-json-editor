f7f598cd7281a8f463f95f4b6f376c431ef485dc
dsymonov <dsymonov@justcoded.co>
2020-12-06 09:40:50 +0200

    update example - add lib json pretty

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="wrapper">
-    <h1 class="page-title">vue-json-editor</h1>
+    <h1 class="page-title">Vue JSON visual editor</h1>
     <div class="container">
-      <div class="editor">
+      <div class="json-editor">
         <json-editor
           :options="{
             confirmText: 'confirm',
@@ -12,93 +12,45 @@
           v-model="jsonData"
         ></json-editor>
       </div>
-      <div class="code">
-        <code>{{ codeJson }}</code>
+      <div class="json-output">
+        <vue-json-pretty :data="jsonData"> </vue-json-pretty>
       </div>
     </div>
   </div>
 </template>
 <script>
+import VueJsonPretty from 'vue-json-pretty';
+
 export default {
   name: 'app',
+  components: {
+    VueJsonPretty,
+  },
   data() {
     return {
       jsonData: {
-        name: 'Jack',
-        age: 23,
+        name: 'Taras',
+        age: 28,
         address: [
-          'Iceland',
+          'Ukraine',
           {
-            city: 'Reykjavík',
+            city: 'Kharkiv',
           },
         ],
         ohters: {
           id: 1246,
           time: new Date(),
-          description: 'another',
-          pattern: /daf/g,
+          description: 'last example update',
+          pattern: /abc/g,
         },
       },
     };
   },
-  methods: {
-    //JSON format print
-    formatJson(txt, compress) {
-      var indentChar = '  ';
-      if (/^\s*$/.test(txt)) {
-        console.error('The data is empty and cannot be formatted!');
-        return;
-      }
-      try {
-        var data = eval('(' + txt + ')');
-      } catch (e) {
-        throw `Data source syntax error, formatting failed! Error message: ${e.description}`;
-        return;
-      }
-      var draw = [],
-        last = false,
-        This = this,
-        line = compress ? '' : '\n',
-        nodeCount = 0,
-        maxDepth = 0;
-
-      var notify = function (name, value, isLast, indent, formObj) {
-        nodeCount++;
-        for (var i = 0, tab = ''; i < indent; i++) tab += indentChar;
-        tab = compress ? '' : tab;
-        maxDepth = ++indent;
-        if (value && value.constructor == Array) {
-          draw.push(tab + (formObj ? '"' + name + '":' : '') + '[' + line);
-          for (var i = 0; i < value.length; i++)
-            notify(i, value[i], i == value.length - 1, indent, false);
-          draw.push(tab + ']' + (isLast ? line : ',' + line));
-        } else if (value && typeof value == 'object') {
-          draw.push(tab + (formObj ? '"' + name + '":' : '') + '{' + line);
-          var len = 0,
-            i = 0;
-          for (var key in value) len++;
-          for (var key in value) notify(key, value[key], ++i == len, indent, true);
-          draw.push(tab + '}' + (isLast ? line : ',' + line));
-        } else {
-          if (typeof value == 'string') value = '"' + value + '"';
-          draw.push(tab + (formObj ? '"' + name + '":' : '') + value + (isLast ? '' : ',') + line);
-        }
-      };
-      var isLast = true,
-        indent = 0;
-      notify('', data, isLast, indent, false);
-
-      return draw.join('');
-    },
-  },
-  computed: {
-    codeJson() {
-      return this.formatJson(JSON.stringify(this.jsonData));
-    },
-  },
 };
 </script>
 <style lang="scss">
+@import 'vue-json-pretty/lib/styles.css';
+
 .page-title {
   text-align: center;
 }
@@ -108,12 +60,9 @@ export default {
   gap: 40px;
   padding: 30px;
 }
-.code {
+.json-output {
   padding: 20px;
   border-radius: 5px;
   box-shadow: 1px 3px 10px rgba(0, 0, 0, 0.25);
-  .json {
-    white-space: pre-wrap;
-  }
 }
 </style>
